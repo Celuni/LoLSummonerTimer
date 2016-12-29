4540c07d990dbb145984ea6573829177c2ee2af4
Will Wen <will.wen1@gmail.com>
2016-12-29 02:09:48 -0500

    apply git ignore

--- loltimer/EntryPoint.xaml.cs
+++ loltimer/EntryPoint.xaml.cs
@@ -7,6 +7,7 @@ using Loltimer.ChampionTimer;
 using System.Collections.Generic;
 using RiotSharp;
 using RiotSharp.SummonerEndpoint;
+using System.Collections;
 
 namespace Loltimer
 {
@@ -21,6 +22,9 @@ namespace Loltimer
         private DragAndDrop dnd;
         private List<ChampionTimerPresenter> championPanelPresenters;
         private HotKeyManagerPresenter hotkeyManager;
+        //pls no steal my api key....
+        RiotApi api = RiotApi.GetInstance("1cc36623-f0aa-4a42-a3f1-942c0da13e7f");
+        StaticRiotApi staticApi = StaticRiotApi.GetInstance("1cc36623-f0aa-4a42-a3f1-942c0da13e7f");
 
         public MainWindow()
         {
@@ -40,7 +44,7 @@ namespace Loltimer
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            ptr = NativeMethods.FindWindow(null,this.DataContext.ToString());
+            ptr = NativeMethods.FindWindow(null, this.DataContext.ToString());
             hotkeyManager = new HotKeyManagerPresenter(ptr);
         }
 
@@ -118,29 +122,104 @@ namespace Loltimer
             GlobalVars.secondsOnly = !GlobalVars.secondsOnly;
         }
 
-        private void SummonerNameTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void SummonerNameTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            string championName = SummonerNameTextBox.Text;
-            var api = RiotApi.GetInstance("1cc36623-f0aa-4a42-a3f1-942c0da13e7f");
-            Summoner summoner;
-            try
+
+            if (e.Key == System.Windows.Input.Key.Return)
             {
-                summoner = api.GetSummoner(Region.na, championName);
-                var currentGame = api.GetCurrentGame(Platform.NA1, summoner.Id);
-                //int friendlyID = currentGame.Participants ;
-                foreach (var p in currentGame.Participants){
-                    Console.WriteLine(p);
+                string summonerName = SummonerNameTextBox.Text;
+
+                Summoner summoner;
+                RiotSharp.CurrentGameEndpoint.CurrentGame currentGame;
+                try
+                {
+                    summoner = api.GetSummoner(Region.na, summonerName);
                 }
+                catch (RiotSharpException ex)
+                {
+                    System.Windows.Forms.MessageBox.Show("That Summoner does not exist");
+                    return;
+                }
+                try
+                {
+                    currentGame = api.GetCurrentGame(Platform.NA1, summoner.Id);
+                }
+                catch (RiotSharp.RiotSharpException ex)
+                {
+                    //Handle the exception however you want.
+
+                    System.Windows.Forms.MessageBox.Show("Could not find that summoner in a game");
+                    return;
+                }
+                List<RiotSharp.CurrentGameEndpoint.Participant> team1 = new List<RiotSharp.CurrentGameEndpoint.Participant>();
+                List<RiotSharp.CurrentGameEndpoint.Participant> team2 = new List<RiotSharp.CurrentGameEndpoint.Participant>();
+                int teamIDofSummoner = -1;
+                //split the participants into their teams, ally and enemy
+                foreach (var p in currentGame.Participants)
+                {
+                    if (summoner.Name.Equals(p.SummonerName))
+                    {
+                        teamIDofSummoner = (int)p.TeamId;
+                    }
+                    if (p.TeamId == 100) //ids are 100 or 200
+                    {
+                        team1.Add(p);
+                    }
+                    else
+                    {
+                        team2.Add(p);
+                    }
+                }
+
+                Console.WriteLine("Main summoner's enemies are: ");
+                List<RiotSharp.CurrentGameEndpoint.Participant> enemyTeam;
+                if (teamIDofSummoner == 100)
+                    enemyTeam = team2;
+                else
+                    enemyTeam = team1;
+
+                foreach (var p in enemyTeam)
+                {
+                    Console.WriteLine("Player " + p.SummonerName + " is playing " + staticApi.GetChampion(Region.na, (int)p.ChampionId).Name);
+
+
+                    if (staticApi.GetSummonerSpell(Region.na,
+                        (RiotSharp.StaticDataEndpoint.SummonerSpell)p.SummonuerSpell1).Name.Equals("Flash") ||
+                        staticApi.GetSummonerSpell(Region.na,
+                        (RiotSharp.StaticDataEndpoint.SummonerSpell)p.SummonerSpell2).Name.Equals("Flash"))
+                    {
+                        Console.WriteLine("and they have Flash.");
+                    }
+
+                    /**
+                     * masteryId	long	The ID of the mastery
+                     * rank	        int	    The number of points put into this mastery by the user
+                     */
+                    var m = p.Masteries;
+                    foreach (var mastery in m)
+                    {
+                        var masteryDetail = staticApi.GetMastery(Region.na, (int) mastery.MasteryId);
+                        if (masteryDetail.Name.Equals("Insight")) //also id == 6241
+                        {
+
+                            int id = masteryDetail.Id;
+                            Console.WriteLine("And they have the insight mastery.");
+                        }
+
+                        
+                    }
+
+                }
+
+
+
+
             }
-            catch (RiotSharpException ex)
-            {
-                //Handle the exception however you want.
-            }
-           
+
 
 
         }
-
     }
+
 }
 
